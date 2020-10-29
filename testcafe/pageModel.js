2502d35ee5fe2bf30c83bd039d0bb86c24dde467
Leys <papitasleys@aol.com>
2020-10-29 13:08:22 -0600

    es la practica 8, hacer login

--- testcafe/pageModel.js
+++ testcafe/pageModel.js
@@ -3,44 +3,46 @@ import { Selector } from 'testcafe';
 class Page {
     constructor() {
         //practica 2
-        this.link2 = Selector('a').withText('A/B Testing')
-        this.text21 = Selector('#content > div > h3')
+        this.link2 = Selector('a').withText('A/B Testing');
+        this.text21 = Selector('#content > div > h3');
 
         //practica 3
-        this.linkForgotPassword = Selector('a').withText('Forgot Password')
-        this.inputEmail = Selector('#email')
-        this.buttonRetrievePassword = Selector('#form_submit')
-        this.textRetrieveResult = Selector('#content')
-        this.textServerError = Selector('h1').withText('Internal Server Error')
+        this.linkForgotPassword = Selector('a').withText('Forgot Password');
+        this.inputEmail = Selector('#email');
+        this.buttonRetrievePassword = Selector('#form_submit');
+        this.textRetrieveResult = Selector('#content');
+        this.textServerError = Selector('h1').withText('Internal Server Error');
 
         //practoca 4
-        this.linkCheckboxes = Selector('a').withText('Checkboxes')
+        this.linkCheckboxes = Selector('a').withText('Checkboxes');
         //checkbox padre
-        this.baseCheckbox = Selector('#checkboxes  > input[type=checkbox]')
+        this.baseCheckbox = Selector('#checkboxes  > input[type=checkbox]');
         //checkbox hijos
         this.checkboxFirst = Selector(
             '#checkboxes > input[type=checkbox]:nth-child(1)'
-        )
+        );
         this.checkboxSecond = Selector(
             '#checkboxes > input[type=checkbox]:nth-child(3)'
-        )
+        );
 
         //proctica 5
-        this.linkInputs = Selector('a').withText('Inputs')
+        this.linkInputs = Selector('a').withText('Inputs');
         this.inputNumbers = Selector(
             '#content > div > div > div > input[type=number]'
-        )
+        );
 
         //parctica 6
-        this.linkRemoveElements = Selector('a').withText('Add/Remove Elements')
-        this.buttonAddElement = Selector('#content > div > button')
-        this.baseButtonRemove = Selector('#elements >  button[class=added-manually]')
+        this.linkRemoveElements = Selector('a').withText('Add/Remove Elements');
+        this.buttonAddElement = Selector('#content > div > button');
+        this.baseButtonRemove = Selector(
+            '#elements >  button[class=added-manually]'
+        );
 
         //platica 7
         this.inputFirstName = Selector('#first-name');
         this.inputLastName = Selector('#last-name');
         this.inputJobTitle = Selector('#job-title');
-        this.radiobuttonHighSchool= Selector('#radio-button-1');
+        this.radiobuttonHighSchool = Selector('#radio-button-1');
         this.radiobuttonCollege = Selector('#radio-button-2');
         this.radiobuttonGradSchool = Selector('#radio-button-3');
         this.checkboxMale = Selector('#checkbox-1');
@@ -49,12 +51,22 @@ class Page {
         this.selectExperience = Selector('#select-menu');
         this.optionExperience = Selector('#select-menu > option:nth-child(5)');
         this.datepickerDate = Selector('#datepicker');
-        this.buttonSubmitForm = Selector('body > div > form > div > div:nth-child(15) > a');
-        this.textSubmitTitle = Selector('h1').withText('Thanks for submitting your form');
+        this.buttonSubmitForm = Selector(
+            'body > div > form > div > div:nth-child(15) > a'
+        );
+        this.textSubmitTitle = Selector('h1').withText(
+            'Thanks for submitting your form'
+        );
 
         // pratcaca 8 (login)
+        this.linkLogin = Selector('a').withText('Form Authentication');
+        this.headerLogin = Selector('#content > div > h2');
+        this.inputUsername = Selector('#username');
+        this.inputPassword = Selector('#password');
+        this.buttonSubmitLogin = Selector('#login > button');
+        this.textLoginSuccess = Selector('#flash');
 
-        //practica 9 
+        //practica 9
         this.linkKeyPress = Selector('a').withText('Key Presses');
         this.textKeyPressResult = Selector('#result');
 
@@ -63,7 +75,7 @@ class Page {
         this.hotspotPopo = Selector('#hot-spot');
 
         //practica11
-        this.linkSlider = Selector ('a').withText('Horizontal Slider');
+        this.linkSlider = Selector('a').withText('Horizontal Slider');
         this.sliderMain = Selector('#content > div > div > input[type=range]');
         this.rangeValue = Selector('#range');
     }
--- testcafe/practica08.js
+++ testcafe/practica08.js
@@ -2,3 +2,22 @@
     hacer la pagina de login
 */ 
 
+import { selector } from "testcafe";
+import page from "./pageModel";
+
+fixture('Práctica 8 con testcafe')
+    .page('https://the-internet.herokuapp.com/');
+
+test('Test prueba login', async t => {
+    await t
+        .click(page.linkLogin)
+    
+    await t
+        .expect(page.headerLogin.visible).ok()
+        .typeText(page.inputUsername, 'tomsmith', {speed : 0.5})
+        .expect(page.inputUsername).eql('tomsmith')
+        .typeText(page.inputPassword, 'SuperSecurePassword!', {speed : 0.5})
+        .expect(page.inputPassword).eql('●●●●●●●●●●●●●●●●●●●●')
+        .click(page.buttonSubmitLogin)
+        .expect(page.textLoginSuccess.innerText).eql('You logged into a secure area!')
+});
